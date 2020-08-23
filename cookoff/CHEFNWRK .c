6230de596ff080793da81eaf562421c3735f133b
Amol Satsangi <15amolsat@.com>
2020-08-23 23:45:50 +0530

    Code pushed

---
+++ cookoff/CHEFNWRK .c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n,k,s,j=1;
+        scanf("%d",&n);
+        scanf("%d",&k);
+        int a[n],i;
+        for(i=0;i<n;i++)
+        {
+            scanf("%d",&a[i]);
+        }
+        s=0;
+        for(i=0;i<n;i++)
+        {
+            s=s+a[i];
+            if(s>k)
+            {
+                j++;
+                s=a[i];
+            }
+            if(k<a[i])
+            {
+                j=-1;
+                break;
+            }
+        }
+        printf("%d\n",j);
+        
+    }
+    return 0;
+}
+
