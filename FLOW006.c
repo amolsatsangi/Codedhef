40b0824153166177d79e5e6a281084a98ee29efd
AMOL SATSANGI <57009387+amolsatsangi@users.noreply.github.com>
2020-08-24 21:33:27 +0530

    Create FLOW006.c

---
+++ FLOW006.c
@@ -0,0 +1,45 @@
+/********
+SUM OF DIGITS
+You're given an integer N. Write a program to calculate the sum of all the digits of N.
+
+Input
+The first line contains an integer T, the total number of testcases. Then follow T lines, each line contains an integer N.
+
+Output
+For each test case, calculate the sum of digits of N, and display it in a new line.
+
+Constraints
+1 ≤ T ≤ 1000
+1 ≤ N ≤ 1000000
+Example
+Input
+3 
+12345
+31203
+2123
+Output
+15
+9
+8
+********/
+
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        long int n;
+        int i,s=0;
+        scanf("%ld",&n);
+        while(n>9)
+        {
+            s=s+n%10;
+            n=n/10;
+        }
+        s=s+n;
+        printf("%d\n",s);
+    }
+    return 0;
+}
